cc324e1ef97706b6756df569e8c17349e9cfbfa2
Ovilares <71732613+Ovilares@users.noreply.github.com>
2020-11-03 18:38:57 -0600

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,1 +1,30 @@
+/* Oliver Josué Padilla Quintanilla*/
+
+#ifndef Punto_H
+#define Punto_H
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+class Punto2D
+{
+public:
+
+    float SetPosicion(float a, float b);
+    void Trasladar(float a, float b);
+    void RotarRespectoAlOrigen(float a);
+    void Escalar(float a, float b);
+    float GetX();
+    float GetY();
+
+    // Escalar : Escala la posición del punto en el plano, como parámetros recibe el factor de escala sobre cada eje, primero sobre x y luego sobre y.
+
+private:
+	float x;
+	float y;
+};
+
+#endif
 
