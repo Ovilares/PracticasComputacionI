f3d0b52c5839c3320665218abfa4a6d94d8e4f2f
Ovilares <71732613+Ovilares@users.noreply.github.com>
2020-11-03 18:39:38 -0600

    Update Punto.cpp

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -1,1 +1,41 @@
+/* Oliver Josué Padilla Quintanilla*/
 
+#include <iostream>
+#include <array>
+
+#include "Punto.h"
+
+using namespace std;
+
+float Punto2D::SetPosicion(float a, float b)
+{
+	x = a;
+	y = b;
+}
+
+void Punto2D::Trasladar(float a, float b)
+{
+	x += a;
+	y += b;
+}
+
+void Punto2D::RotarRespectoAlOrigen(float a)
+{
+	double grad = 0.9998
+}
+
+void Punto2D::Escalar(float a, float b)
+{
+	x = a * x;
+	y = b * y;
+}
+
+float Punto2D::GetX()
+{
+	return x;
+}
+
+float Punto2D::GetY()
+{
+	return y;
+}
