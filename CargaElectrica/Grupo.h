5eec44c3c84bba2dd5d00e4627d35bf8074f8838
Ovilares <71732613+Ovilares@users.noreply.github.com>
2020-11-19 15:28:37 -0600

    Update Grupo.h

--- CargaElectrica/Grupo.h
+++ CargaElectrica/Grupo.h
@@ -1,1 +1,26 @@
 
+#ifndef GRUPO_H
+#define GRUPO_H
+
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+class Grupo
+{
+public:
+	Grupo() { nombreGrupo = " "; pkGrupo = 0; cargaGrupo = 0; };
+	Grupo(string nom, float pkG, int cG) { nombreGrupo = nom; pkGrupo = pkG; cargaGrupo = cG; }
+	float getPk();
+	void anadirCarga();
+	int getCarga();
+
+private:
+	string nombreGrupo;
+	float pkGrupo;
+	int cargaGrupo;
+};
+
+#endif
