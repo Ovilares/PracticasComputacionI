1ad214c8bc2e3e17b4805c03030cbaee52d17789
Ovilares <71732613+Ovilares@users.noreply.github.com>
2021-01-14 17:20:20 -0600

    Update GaussJordanP.py

--- GaussJordanP/GaussJordanP.py
+++ GaussJordanP/GaussJordanP.py
@@ -4,42 +4,40 @@
 Oliver Josué Padilla Quintanilla
 """
 
-''' Llenar la matriz de ecuaciones '''
+''' Instanciar Matriz'''
 ec = int(input("Número de ecuaciones: "))
 col= ec+1
 MatEc= list()
+print('\n')
+
+''' Llenar Matriz '''
 for i in range(0, ec):
   MatEc.append([])
   for j in range(0, col):
     MatEc[i].append(float(input(f"Valor elemento [{i}][{j}]: ")))
-print(MatEc)
 
-''' Método de Gauss '''
+''' Eliminación de Gauss '''
 for i in range(0,ec):
   a = MatEc[i][i]
   for j in range (0, col):
     MatEc[i][j] = MatEc[i][j]/a
-  print(MatEc)
   for k in range(i+1, ec):
     b = MatEc[k][i]*-1
-    print("B= ", b)
     for l in range(0, col):
       z = MatEc[i][l]*b
-      print("Z= ", z)
       MatEc[k][l] = z+MatEc[k][l]
-print(MatEc)
 
 ''' Método de Jordan '''
 for i in range(-1, -col, -1):
   for k in range(i-1, -col, -1):
     b = MatEc[k][i-1]*-1
-    print("B= ", b)
     for j in range(-1, -col-1, -1):
       z = MatEc[i][j]*b
-      print("Z= ", z)
       MatEc[k][j] = z+MatEc[k][j]
-print(MatEc)
 
-''' Impresión de resultados '''
+''' Imprimir soluciones '''
+print('\n')
+print("Solución:")
 for i in range(0, ec):
   print(f"x{i} =", MatEc[i][ec])
+  
