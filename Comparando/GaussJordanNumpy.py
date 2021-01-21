87a9fce9cfec5a3ccb6fb9ef90302f84dfc215e0
Ovilares <71732613+Ovilares@users.noreply.github.com>
2021-01-20 19:11:50 -0600

    Update GaussJordanNumpy.py

--- Comparando/GaussJordanNumpy.py
+++ Comparando/GaussJordanNumpy.py
@@ -1,1 +1,64 @@
 
+# -*- coding: utf-8 -*-
+"""
+Gauss Jordan Numpy
+Oliver Josué Padilla Quintanilla
+"""
+
+import numpy as np
+
+''' Instanciar Matriz'''
+ec = int(input("Número de ecuaciones: "))
+col= ec+1
+MatEc= np.zeros((ec,col), dtype= np.float32)
+stop = 0
+
+''' Llenar Matriz '''
+for x in range(0, ec):
+  for y in range(0, col):
+    MatEc[x][y] = float(input(f"Valor elemento [{x}][{y}]: "))
+
+''' Eliminación de Gauss '''
+for i in range(0,ec):
+  a = MatEc[i][i]
+  ''' Cambiar renglones con 0s'''
+  while a ==0:
+    for c in range(i+1,ec):
+      MatEc[[i,c]]=MatEc[[c,i]]
+      a = MatEc[i][i]
+      if a != 0:
+        break
+    if a == 0:
+      stop = 1
+      break
+
+  ''' Continuación '''
+  if stop == 1:
+    break
+  for j in range (0, col):
+    MatEc[i][j] = MatEc[i][j]/a
+  for k in range(i+1, ec):
+    b = MatEc[k][i]*-1
+    for l in range(0, col):
+      z = MatEc[i][l]*b
+      MatEc[k][l] = z+MatEc[k][l]
+
+''' Método de Jordan '''
+
+for i in range(-1, -col, -1):
+  if stop ==1:
+    break
+  for k in range(i-1, -col, -1):
+    b = MatEc[k][i-1]*-1
+    for j in range(-1, -col-1, -1):
+      z = MatEc[i][j]*b
+      MatEc[k][j] = z+MatEc[k][j]
+
+''' Imprimir soluciones -'''
+if stop==1:
+  print("\n No tiene solución")
+else:
+  print('\n')
+  print("Solución:")
+  for i in range(0, ec):
+    print(f"x{i} =", MatEc[i][ec])
